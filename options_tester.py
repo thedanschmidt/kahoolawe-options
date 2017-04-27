9a1376fed88232efa9addf7dc58339b2a5baf095
Dan Schmidt <danielschmidt24@gmail.com>
2017-04-27 10:55:17 -0700

    src file commit

--- options_tester.py
+++ options_tester.py
@@ -2,5 +2,22 @@ import numpy as np
 import pandas as pd
 import math
 
+# TODO convert models 
+#       -Black-Scholes, Rachel 
+#       -roll dice, Andrew 
+#       -propogation model, Dan 
 
 
+# Example pricing interface
+def bs_price_option(strike, underlying, expiration_date, side):
+    alpha = 0.01
+    volatility= 0.02 
+    return black_scholes()
+
+
+# Function that takes a pricing model and calculates its error in predicting
+# a range of options values
+# - Jon
+def compute_model_error(model, symbol_name, side, date_list=None, date_range=None):
+    
+    return 
